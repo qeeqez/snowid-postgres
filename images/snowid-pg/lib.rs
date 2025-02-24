8390170f30070e8b1eade4755b1481f7bf5d1be7
Maksim Sasnouski <qeeqez@gmail.com>
2025-02-24 05:24:12 +0400

    Cache prebuild with this dummy file

---
+++ images/snowid-pg/lib.rs
@@ -0,0 +1,14 @@
+#![allow(unexpected_cfgs)]
+#[allow(unused_imports)]
+
+use heapless::FnvIndexMap;
+use pgrx::atomics::*;
+use pgrx::lwlock::PgLwLock;
+use pgrx::pg_shmem_init;
+use pgrx::prelude::*;
+use pgrx::shmem::PGRXSharedMemory;
+use pgrx::shmem::*;
+use snowid::SnowID;
+use std::sync::atomic::{AtomicI16, Ordering}; // Import PgSharedMemory directly
+
+pg_module_magic!();
