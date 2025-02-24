d710ad82269a50dd280d49643cf276ddab1605ae
Maksim Sasnouski <qeeqez@gmail.com>
2025-02-24 18:35:50 +0400

    Add stats function

--- src/lib.rs
+++ src/lib.rs
@@ -72,57 +72,40 @@ fn gen_snowid(table_name: &str) -> i64 {
     generator.0.generate().try_into().unwrap()
 }
 
-// TODO: tests cant run without shared_preloaded_library
-// #[cfg(any(test, feature = "pg_test"))]
-// #[pg_schema]
-// mod tests {
-//     use super::*;
-//
-//     #[pg_test]
-//     fn test_node_id() {
-//         // Test setting and getting node ID
-//         crate::set_node_id(5);
-//         assert_eq!(5, crate::get_node_id());
-//     }
-//
-//     #[pg_test]
-//     fn test_gen_snowid_not_equal() {
-//         // Test generating IDs for different tables
-//         set_node_id(1);
-//         let id1 = crate::gen_snowid("table1");
-//         let id2 = crate::gen_snowid("table1");
-//         let id3 = crate::gen_snowid("table1");
-//
-//         // IDs should be different
-//         assert_ne!(id1, id2);
-//         assert_ne!(id1, id3);
-//         assert_ne!(id2, id3);
-//     }
-//
-//     #[pg_test]
-//     fn test_gen_snowid_equal() { //FAILS
-//         // Test generating IDs for different tables
-//         set_node_id(1);
-//         let id1 = crate::gen_snowid("table1");
-//         let id2 = crate::gen_snowid("table2");
-//         let id3 = crate::gen_snowid("table3");
-//
-//         // IDs should be different
-//         assert_eq!(id1, id2);
-//         assert_eq!(id1, id3);
-//         assert_eq!(id2, id3);
-//     }
-//
-//     // #[pg_test]
-//     // fn test_get_snowid_timestamp() {
-//     //     // Test generating IDs for different tables
-//     //     crate::set_node_id(1);
-//     //     let timestamp = crate::get_snowid_timestamp(151900616753418240, "table1");
-//     //
-//     //     // IDs should be different
-//     //     assert_eq!(timestamp, 36215929211);
-//     // }
-// }
+/// Extracts the timestamp from a Snowflake ID
+#[pg_extern]
+fn get_snowid_timestamp(id: i64, table_name: &str) -> i64 {
+    let generators = GENERATORS.exclusive();
+    let generator = &generators[&table_name.to_string()];
+    let id_u64: u64 = id.try_into().unwrap();
+    generator.0.extract.timestamp(id_u64).try_into().unwrap()
+}
+
+#[pg_extern]
+fn snowid_stats() -> String {
+    let generators = GENERATORS.share();
+    let mut stats = String::from("SnowID Statistics:\n");
+    stats.push_str(&format!("Total Generators: {}\n", generators.len()));
+    stats.push_str("Generators:\n");
+
+    for (key, _) in generators.iter() {
+        stats.push_str(&format!(
+            "- Key: '{}', bytes: {:?}\n",
+            key,
+            key.as_bytes()
+        ));
+    }
+
+    stats.push_str(&format!(
+        "Current Node ID: {}\n\
+         Max Tables Supported: {}",
+        NODE_ID.get().load(Ordering::Relaxed),
+        MAX_TABLES
+    ));
+
+    log!("{}", stats);
+    stats
+}
 
 /// This module is required by `cargo pgrx test` invocations.
 /// It must be visible at the root of your extension crate.
