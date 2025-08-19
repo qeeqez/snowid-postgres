e65ba6ef7f850cdceb910954aee23ddc8c7f5eee
Maksim Sasnouski <qeeqez@gmail.com>
2025-08-19 18:39:19 +0400

    Update dummy libs #86euj7vtb

--- images/dev/dummy.rs
+++ images/dev/dummy.rs
@@ -2,11 +2,12 @@
 #![allow(unused_imports)]
 
 use heapless::FnvIndexMap;
-use pgrx::atomics::*;
+use pgrx::atomics::PgAtomic;
 use pgrx::lwlock::PgLwLock;
 use pgrx::pg_shmem_init;
 use pgrx::prelude::*;
 use pgrx::shmem::PGRXSharedMemory;
-use pgrx::shmem::*;
+use pgrx::shmem::AssertPGRXSharedMemory;
 use snowid::SnowID;
+use std::ffi::CStr;
 use std::sync::atomic::{AtomicI16, Ordering};
