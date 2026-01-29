8421cf0ad8ad494bfdb65963f14e9dd10ee3da1e
Maksim Sasnouski <qeeqez@gmail.com>
2026-01-30 00:26:51 +0300

    fix(release): proper version set

--- sql/pg_snowid--0.7.0--1.0.1.sql
+++ sql/pg_snowid--0.7.0--1.0.1.sql
@@ -1,2 +1,2 @@
--- Upgrade script for pg_snowid from version 0.7.0 to 1.0.0
+-- Upgrade script for pg_snowid from version 0.7.0 to 1.0.1
 -- No SQL schema changes in this version. Underlying library updated.
