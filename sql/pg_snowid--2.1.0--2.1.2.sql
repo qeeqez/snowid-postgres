a2e81271f92feb3b44d058a5b3263836c39866cb
Maksim Sasnouski <qeeqez@gmail.com>
2026-02-27 00:36:29 +0400

    fix: improve build speed

--- sql/pg_snowid--2.1.0--2.1.2.sql
+++ sql/pg_snowid--2.1.0--2.1.2.sql
@@ -1,2 +1,2 @@
--- Upgrade script for pg_snowid from version 2.1.0 to 2.1.1
+-- Upgrade script for pg_snowid from version 2.1.0 to 2.1.2
 -- No SQL schema changes in this version. Underlying library updated.
