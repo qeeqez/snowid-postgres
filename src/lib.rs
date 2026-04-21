1d8d98807e4a0395df91b07ef30f7a04d19afbc1
Maksim Sasnouski <qeeqez@gmail.com>
2026-04-21 12:45:01 +0300

    fix: cleanup dev dockerfile and format code

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,8 @@ use pgrx::atomics::PgAtomic;
 use pgrx::lwlock::PgLwLock;
 use pgrx::pg_shmem_init;
 use pgrx::prelude::*;
-use pgrx::shmem::PGRXSharedMemory;
 use pgrx::shmem::AssertPGRXSharedMemory;
+use pgrx::shmem::PGRXSharedMemory;
 use snowid::SnowID;
 use std::sync::atomic::{AtomicI16, Ordering};
 
